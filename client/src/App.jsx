cb7ab2195a3e29160e0b9cf83fe9205c8ad71b16
Puttapong <puttapong.lekk@gmail.com>
2024-06-07 15:00:28 +0700

    React Data Fetching Products Assignment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,59 @@
 import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState();
+
+  const getProducts = async () => {
+    const result = await axios.get("http://localhost:4001/products");
+    // console.log(result);
+    setProducts(result.data.data);
+  };
+
+  const handleDelete = async (id) => {
+    const result = await axios.delete(`http://localhost:4001/products/${id}`);
+    getProducts();
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
-      <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
+      {products.map((item) => {
+        return (
+          <div className="product-list">
+            <div className="product">
+              <div className="product-preview">
+                <img
+                  src={item.image}
+                  alt="some product"
+                  width="350"
+                  height="350"
+                />
+              </div>
+              <div className="product-detail">
+                <h1>Product name: {item.name} </h1>
+                <h2>Product price: {item.price} Baht</h2>
+                <p>Product description: {item.description}</p>
+              </div>
 
-          <button className="delete-button">x</button>
-        </div>
-      </div>
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(item.id)}
+              >
+                x
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
